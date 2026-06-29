b20944a6782270547a11c9cf314ae86cec29cf21
Oldrich Vetesnik <oldrich.vetesnik@gmail.com>
2026-06-29 11:32:51 +0200

    JS tune

--- public/js/dragdrop.js
+++ public/js/dragdrop.js
@@ -2,28 +2,28 @@ class Dragdrop {
   constructor() {
     $(".js-draggable").draggable({
       // revert: true
-      revertDuration: 200
-    })
+      revertDuration: 200,
+    });
 
     $(".js-droppable").droppable({
       accept: ".js-draggable",
       tolerance: "intersect",
       drop: (e, ui) => {
-        const $slot = $(e.target)
-        const $battery = ui.draggable
-        const batteryId = $battery.data("id")
+        const $slot = $(e.target);
+        const $battery = ui.draggable;
+        const batteryId = $battery.data("id");
 
-        let url = $slot.data("accept-url")
-        url = url.replace(":battery_id", batteryId)
+        let url = $slot.data("accept-url");
+        url = url.replace(":battery_id", batteryId);
 
         if ($slot.hasClass("battery-item") && confirm(`Vyměnit baterii ${$slot.find(".battery-name").text()}?`)) {
-          window.location = url
+          window.location = url;
         } else if (!$slot.hasClass("battery-item")) {
-          window.location = url
+          window.location = url;
         }
-      }
-    })
+      },
+    });
   }
 }
 
-window.Dragdrop = Dragdrop
+window.Dragdrop = Dragdrop;
